e52bb70cc435179d0a56ca675f51814a61c0fd03
aditigund2004 <aditigund19@gmail.com>
2026-05-26 21:42:30 +0530

    Js Practice Session

---
+++ practice.js
@@ -0,0 +1,28 @@
+// var was the original way to declare variables but has confusing behaviors.
+var a = 9;  // declaration 
+
+a = 10 // re-assign 
+
+// a = 'ram'
+
+console.log(a);
+
+// var var = 'hello'
+
+
+
+// Use let for variables whose value you expect to change (e.g., counters in a loop or toggle states).
+let b = 10;
+
+b = 5
+
+console.log(b)
+
+
+
+// Use const for values that should not be reassigned after they are first set.
+const c = 10;
+
+// const c = 1
+
+console.log(c)
